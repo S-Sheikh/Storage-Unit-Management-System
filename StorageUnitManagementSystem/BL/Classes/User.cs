81d51e213b6350cbb728a3d7eb8681acc02a1e40
Vevan <vevan66@outlook.com>
2016-11-10 14:30:01 +0200

    User class fixed

--- StorageUnitManagementSystem/BL/Classes/User.cs
+++ StorageUnitManagementSystem/BL/Classes/User.cs
@@ -8,5 +8,47 @@ namespace StorageUnitManagementSystem.BL.Classes
 {
     class User
     {
+        private string userName;
+        private string password;
+        private string role;
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+
+            set
+            {
+                userName = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public string Role
+        {
+            get
+            {
+                return role;
+            }
+
+            set
+            {
+                role = value;
+            }
+        }
     }
 }
